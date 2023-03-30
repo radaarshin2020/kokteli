e5b9c8ce04ba0705dba7502d7aca46b15c120009
radaarshin2020 <arshinkovska@yahoo.com>
2023-03-30 10:45:27 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, facere. Error deserunt, 
+        rerum vel sed tempore sapiente impedit maiores consectetur sequi quasi. Quaerat quo eius 
+        deserunt quis iste maiores modi harum, iure, iusto accusantium voluptatem? Optio saepe sint dolorum? Voluptate?
+      </p>
+    </section>
+  )
+}
+
+export default About
